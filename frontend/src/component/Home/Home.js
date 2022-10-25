518113183425432dad4cc7b36c8fa63ab4b97faa
SwapnilV26 <swapnilvish2812@gmail.com>
2022-10-26 01:08:34 +0530

    chatbot added

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -7,6 +7,7 @@ import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
+// import Chat from "../chatbot/chat";
 
 const Home = () => {
   const alert = useAlert();
@@ -49,6 +50,7 @@ const Home = () => {
               ))}
           </div>
         </Fragment>
+        
       )}
     </Fragment>
   );
